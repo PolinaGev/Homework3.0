8350ce2b3678aaa40d3e959f938648d44f313180
PolinaG <PolinaG@gmail.com>
2022-08-29 16:44:56 +0300

    Create a task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿void Cube (int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+    if (i >= number)
+    {
+        System.Console.Write(Math.Pow(i, 3) + ".");
+    }
+    else
+    {
+        System.Console.Write(Math.Pow(i, 3) + ", ");
+    }
+    }
+}
+
+Cube(5);
