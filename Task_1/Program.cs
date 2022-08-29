b42fc47e82da475a4cfccaf85f8e7498951a9679
PolinaG <PolinaG@gmail.com>
2022-08-29 16:43:30 +0300

    Create a task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// void Main()
+//         {
+//             int number = 0;
+//             int oldValue = number;
+//             int newValue = 0;
+//             while (number > 0)
+//             {
+//                 int dig = number % 10;
+//                 newValue = newValue * 10 + dig;
+//                 number = number / 10;
+//             }
+//             if (newValue == oldValue)
+//                 Console.WriteLine("Число является палиндромом");
+//             else
+//                 Console.WriteLine("Число не является палиндромом");
+//         }
+// Main();
+
+void GetPolindrom(int number)
+{
+    int oldValue = number;
+    int newValue = 0;
+    while (number > 0)
+    {
+    int dig = number % 10;
+    newValue = newValue * 10 + dig;
+    number = number / 10;
+    }
+    if (newValue == oldValue)
+    Console.WriteLine("Число является палиндромом");
+    else
+    Console.WriteLine("Число не является палиндромом");  
+}
+GetPolindrom(12321);
