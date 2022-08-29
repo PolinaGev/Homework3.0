4bfe9a696c049b389b43a51b014b3ae0d0cb2deb
PolinaG <PolinaG@gmail.com>
2022-08-29 16:43:45 +0300

    Create a task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,7 @@
+﻿double GetRange(int xa, int ya, int xb, int yb, int za, int zb)
+{
+double gfew = Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2);
+return Math.Sqrt(gfew);
+}
+
+System.Console.WriteLine(GetRange(3, 6, 2, 1, 8, -7));
